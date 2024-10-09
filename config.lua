a8ab7086cf37339feb58784293a3e2831ccf7099
Xavitox <94683358+xavitoxdeb@users.noreply.github.com>
2024-10-09 22:47:14 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- Porcentaje de dinero limpio que se devuelve tras el lavado
+Config.Percentage = 0.70 -- 70%
+
+-- Duración de la animación de lavado en segundos
+Config.AnimationTime = 10 -- 10 segundos
+
+-- Tiempo total para completar el lavado de dinero en minutos
+Config.WaitTime = 30 -- 30 minutos
+
+-- Coordenadas del punto de lavado de dinero
+Config.LaunderLocation = vector3(123.4, 456.7, 789.0) -- Cambia esto a la ubicación que quieras
